faa328612031e21f9cfe33e04ca725fde3cf368d
Laura Aupert <aupert.laura@gmail.com>
2021-06-14 19:01:04 -0400

    Installed npms

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Background from "./components/Background";
 
-function App() {
+function App(props) {
+
+  const [bg, setFile] = useState(
+    "https://source.unsplash.com/random/1280x800/?nature"
+  );
+  function onChange(event) {
+    var bgurl = URL.createObjectURL(event.target.files[0]);
+    setFile(bgurl);
+  }
   return (
-    <div className="App">
+    <div className="App" >
+            {/* <Background onChange={onChange}/> */}
+
       <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Welcome to React</h2>
---
+++ client/src/components/Background.js
@@ -0,0 +1,32 @@
+// import { urlencoded } from "express";
+// import React, { useState, useEffect } from "react";
+
+// function Background() {
+// //   const [bg, setBackground] = useState("")
+
+// //   useEffect(() => {
+// //     setBackground(
+// //       "https://source.unsplash.com/random/1280x800/?nature"
+// //     );
+  
+// //   }, [])
+//     // function onChange(event) {
+//     //   var bgurl = URL.createObjectURL(event.target.files[0]);
+//     //   setBackground(bgurl)
+//     // }
+//     return (
+//         <div 
+//     //     style={{backgroundImage: 'url("https://source.unsplash.com/random/1280x800/?nature")'
+//     // }} 
+//     style={{width: "100%"}} style={{height: "100%"}}
+//     >
+//     {/* // style={{width: "600px"}} style={{height: "600px"}}> */}
+
+//         </div>
+//     )
+  
+
+
+// }
+
+// export default Background;
